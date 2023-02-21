cf28cc2e48a8bd75b419cce581b119a8a30f819b
Ahmad <k_han21@yahoo.com>
2023-02-21 17:59:23 +1100

    linear and binary search

--- searching-algorithms/linear-search.js
+++ searching-algorithms/linear-search.js
@@ -1,1 +1,12 @@
-console.log('hi')
+function linearSearch(arr, value) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === value) {
+      return i
+    }
+  }
+
+  return -1
+}
+
+const result = linearSearch([4, 2, 6, 2, 67, 8, 99, 4, 7, 2, 3], 9)
+console.log(result)
